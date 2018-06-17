c8528662402bf1900fee7c19097238de23b4b9ec
Shreyash <shreyashanarase10@gmail.com>
2018-06-17 15:02:50 +0530

    hi

---
+++ facet.py
@@ -0,0 +1,43 @@
+import cv2
+#print(cv2.__file__)
+import time
+
+#it tells whether a face or not
+#point to haar cascase file
+cascPath = "haarcascade_.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+#start the camera
+video_capture = cv2.VideoCapture(0)
+
+#give camera time to warm up
+time.sleep(0.1)
+
+#start video from capture loop
+while True:
+	#take the frame convert it to B&W.
+	#look for facial features
+	ret,frame =video_capture.read()
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor = 1.1,
+		minNeighbors = 5,
+		minSize=(30,30),
+		flags = cv2.CASCADE_SCALE_IMAGE
+		)
+
+		#for each face, draw a green rectangle around it and append to the image
+	for(x,y,w,h) in faces: 
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+		#display the resulting immage
+		cv2.imshow('Video',frame)
+
+		#set "q" as the exit key 
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+#clear the stream capture
+video_capture.release()
+cv2.destroyAllWindows()
